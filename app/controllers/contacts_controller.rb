487a0acc8b561bf7ba4a48d56db200e73c20f8d7
Stas-Buzunko <buzunkostas@gmail.com>
2016-01-04 00:00:15 +0300

    sending mail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,7 @@ class ContactsController < ApplicationController
 	def create
 		@contact = Contact.new(secure_params)
 		if @contact.valid?
+			UserMailer.contact_email(@contact).deliver
 			# TODO save data
 			# TODO send message
 			flash[:notice] = "Message sent from #{@contact.name}"
